9132b08b662ad6dd801807c08a93dd2577d9dacb
Alex Mayer <Alex.Vriley@gmail.com>
2026-03-24 17:23:14 -0600

    Tutorial
    The basics

---
+++ Unjumbler/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+
+    private float num;
+    public GameObject MenuButton2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void closeMenu()
+    {
+        
+    }
+}
