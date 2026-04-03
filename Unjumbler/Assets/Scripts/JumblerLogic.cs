dcfa1cebab8227f01a14d49b583d34381b7667d3
Alex Mayer <Alex.Vriley@gmail.com>
2026-04-03 01:44:08 -0600

    snapTarget logic changes
    snapTargets now instantiates padding to better format and prevent words from being cut off in the layout group.

--- Unjumbler/Assets/Scripts/JumblerLogic.cs
+++ Unjumbler/Assets/Scripts/JumblerLogic.cs
@@ -29,6 +29,8 @@ public class JumblerLogic : MonoBehaviour
     public char [] jumbled;
     bool test;
 
+    public int targetMaxChar;
+
     [SerializeField]
     private GameObject letterPrefab;
     [SerializeField]
@@ -100,6 +102,9 @@ public class JumblerLogic : MonoBehaviour
         return true;
     }
 
+    /* Here we instantiate the Letters that will be used by the user
+     * Each letter contains a text child element to compare when dropped
+     * in the snapTarget Script. */
     public void instantiateDraggableLetters()
     {
         for (int x = 0; x < unjumbled.Length; x++)
@@ -109,14 +114,65 @@ public class JumblerLogic : MonoBehaviour
 
             TMP_Text textComponent = instance.GetComponentInChildren<TMP_Text>();
             textComponent.text = jumbled[x].ToString();
-
-          
         }
     }
+    /* To format the snapTargets (blanks) properly without cutting off the words in the
+     * grid layout group we need to be able to detect if the next word instantiating is
+     * longer than the total length of the line we are on. */
     public void instantiateSnapTarget()
     {
-        for (int x = 0; x < unjumbled.Length; x++)
+        int row = 0;
+
+        for (int x = 0; x< unjumbled.Length; x++)
         {
+            /* If the first character in a row is a space skip it. We do this by setting the 
+             * object inactive in the scene so we can still see the space in the hierarchy */
+            if (row == 0 && unjumbled[x] == ' ')
+            {
+                GameObject skipInstance = Instantiate(snapPrefab, snapSpawn.transform, true);
+                skipInstance.name = "Target " + x;
+
+                TMP_Text skipText = skipInstance.GetComponentInChildren<TMP_Text>();
+                skipText.text = unjumbled[x].ToString();
+
+                skipInstance.SetActive(false); // set it inactive so it doesn't affect the layout
+
+                continue;
+            }
+            // Check to see how long a word is if we are at the start.
+            if (x == 0 || unjumbled[x - 1] == ' ')
+            {
+                int wordLength = 0;
+                int temp = x;
+
+                // We count until we hit a space.
+                while (temp < unjumbled.Length && unjumbled[temp] != ' ')
+                {
+                    wordLength++;
+                    temp++;
+                }
+
+                /* Here we are checking if the current word being instantiated doesn't fit on 
+                 * the current row and if it doesn't we move it to the next row by instantiating
+                 * more blanks. */
+                if (row + wordLength > targetMaxChar && row > 0)
+                {
+                    int padding = targetMaxChar - row;
+                    for (int i = 0; i < padding; i++)
+                    {
+                        GameObject pad = Instantiate(snapPrefab, snapSpawn.transform, true);
+                        pad.name = "Padding";
+
+                        TMP_Text padText = pad.GetComponentInChildren<TMP_Text>();
+                        padText.text = " ";
+                        pad.GetComponent<CanvasRenderer>().SetAlpha(0);
+                    }
+                    row = 0; // Resets the row when we wrap to the next line.
+                }
+            }
+
+            /* Here we instantiate the snapTargets that will be used by the user contaning the text
+             * we need to compare when a user snaps a letter. */
             GameObject instance = Instantiate(snapPrefab, snapSpawn.transform, true);
             instance.name = "Target " + x;
 
@@ -127,7 +183,15 @@ public class JumblerLogic : MonoBehaviour
             {
                 instance.GetComponent<CanvasRenderer>().SetAlpha(0);
             }
+
+            // We keep track of our positon on the grid.
+            row++;
+            if (row >= targetMaxChar)
+            {
+                row = 0;
+            }
         }
+
     }
 }
    
