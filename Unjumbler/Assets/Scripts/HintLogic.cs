fcb3a362f181f67252fdddfcf3a5a732eeb4a1cd
qwualyn <kdegu381@mtroyal.ca>
2026-04-09 23:20:40 -0600

    exit button working, working on hint

--- Unjumbler/Assets/Scripts/HintLogic.cs
+++ Unjumbler/Assets/Scripts/HintLogic.cs
@@ -3,19 +3,24 @@ using UnityEngine.UI;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class HintLogic : MonoBehaviour
 {
+    [SerializeField]
     public JumblerLogic jumbler;
     public char[] unjumbled;
     public char[] author;
     public char[] jumbled;
     public bool allCaps = true; //***FOR CAPITALIZATION LOGIC***
     public char[] upperArr; //***FOR CAPITALIZATION LOGIC***
+    [SerializeField]
     public GameObject hintBox; //need to make UI popup for hint box
     public string hintStr;
+    [SerializeField]
     public Text textElement;
     public char[] hintArr;
+    [SerializeField]
     public SnapTarget checkTarget;
     public string[] colourArr;
     public int hintCount = 0;
@@ -39,6 +44,7 @@ public class HintLogic : MonoBehaviour
     }
     public void hint()
     {
+        //SceneManager.LoadScene();
         hintCount++;
         if (hintCount == 1)
         { // reveal Author name}
--- Unjumbler/Assets/Scripts/exitLogic.cs
+++ Unjumbler/Assets/Scripts/exitLogic.cs
@@ -1,21 +1,30 @@
+using UnityEditor;
+using UnityEngine.UI;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class ExitButton : MonoBehaviour
 {
+    [SerializeField]
     public JumblerLogic jumbler;
     public char[] unjumbled;
     public char[] author;
     public char[] jumbled;
     public bool allCaps = true; //***FOR CAPITALIZATION LOGIC***
     public char[] upperArr; //***FOR CAPITALIZATION LOGIC***
+    [SerializeField]
     public GameObject hintBox; //need to make UI popup for hint box
     public string hintStr;
+    [SerializeField]
+    public Text textElement;
     public char[] hintArr;
+    [SerializeField]
     public SnapTarget checkTarget;
     public string[] colourArr;
     public int hintCount = 0;
-    void exit()
+    void Exit()
     {
         SceneManager.LoadScene("MenuUI");
 
