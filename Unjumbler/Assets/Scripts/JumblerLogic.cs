05eaeb59688a080f90446d4049462f479425308c
milkfan1103 <eodes973@mtroyal.ca>
2026-04-12 21:05:12 -0600

    Added images and colours for the buttons

--- Unjumbler/Assets/Scripts/JumblerLogic.cs
+++ Unjumbler/Assets/Scripts/JumblerLogic.cs
@@ -11,23 +11,12 @@ using TMPro;
 
 public class JumblerLogic : MonoBehaviour
 {
-    public string[] quotes = new [] {"We can only see a short distance ahead, but we " +
-        "can see plenty there that needs to be done.", "That brain of mine is something " +
-        "more than merely mortal; as time will show.", "The most dangerous phrase in the language is, 'We've always done it this way'.", "Hope and curiosity about the future seemed better " +
-        "than guarantees", "In mathematics you don't understand things. You just get used to them.",
-        "I like to learn. That's an art and a science.", "We're having an information explosion, and " +
-        "it's certainly obvious that information is of no use unless it's available", "Logic is the foundation " +
-        "of the certainty of all the knowledge we acquire.", "The world would be a better place if more engineers, " +
-        "like me, hated technology", "I'll bet you don't have a computer in your living room."};
-    public string[] person = new[] {"Alan Turing", "Ada Lovelace", "Grace Hopper",
-        "Hedy Lamarr", "John von Neumann", "Katherin Johnson", "Sister Mary Kenneth Keller",
-        "Leonhard Euler", "Radia Perlman", "Mary Allen Wilkes"};
     
     public char[] unjumbled;
-    public int numQuote = 10;
     public char[] author;
     public char [] jumbled;
     bool test;
+    public QuoteDatabase[] quoteDatabase;
 
     public int targetMaxChar;
 
@@ -62,9 +51,10 @@ public class JumblerLogic : MonoBehaviour
     }
     void stringJumble()
     {
-        int randQuote = Random.Range(0,numQuote);   //picks random quote and author number
-        unjumbled = quotes[randQuote].ToCharArray();
-        author = person[randQuote].ToCharArray();
+        int randQuote = Random.Range(0,quoteDatabase.Length);   //picks random quote and author number
+
+        unjumbled = quoteDatabase[randQuote].quoteText.ToCharArray();
+        author = quoteDatabase[randQuote].quoteAuthor.ToCharArray();
         unjumbled = unjumbled.Concat(" -").ToArray();
         unjumbled = unjumbled.Concat(author).ToArray();
         jumbled = new char[unjumbled.Length];
